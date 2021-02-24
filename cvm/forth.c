48c5375cf48b43a4fd8e7dda59582f5c89f85a95
Virgil Dupras <hsoft@hardcoded.net>
2021-02-24 22:01:01 +0000

    Remove ability to run a Forth source file
    This was for automated tests, which were removed.

--- cvm/forth.c
+++ cvm/forth.c
@@ -14,14 +14,9 @@
 #define WCOLS 80
 #define WLINES 32
 #define STDIO_PORT 0x00
-// This binary is also used for automated tests and those tests, when
-// failing, send a non-zero value to RET_PORT to indicate failure
-#define RET_PORT 0x01
 #define SETX_PORT 0x05
 #define SETY_PORT 0x06
 
-static FILE *fp;
-static int retcode = 0;
 WINDOW *bw, *dw, *w;
 
 void debug_panel()
@@ -35,32 +30,16 @@ void debug_panel()
 static uint8_t iord_stdio()
 {
     int c;
-    if (fp != NULL) {
-        c = getc(fp);
-    } else {
-        debug_panel();
-        c = wgetch(w);
-    }
-    if (c == EOF) {
-        c = 4; // ASCII EOT
-    }
+    debug_panel();
+    c = wgetch(w);
     return (uint8_t)c;
 }
 
 static void iowr_stdio(uint8_t val)
 {
-    if (fp != NULL) {
-        putchar(val);
-    } else {
-        if (val >= 0x20) {
-            wechochar(w, val);
-        }
+    if (val >= 0x20) {
+        wechochar(w, val);
     }
-}
-
-static void iowr_ret(uint8_t val)
-{
-    retcode = val;
 }
 
 static void iowr_setx(uint8_t val)
@@ -83,40 +62,24 @@ int main(int argc, char *argv[])
     }
     vm->iord[STDIO_PORT] = iord_stdio;
     vm->iowr[STDIO_PORT] = iowr_stdio;
-    vm->iowr[RET_PORT] = iowr_ret;
     vm->iowr[SETX_PORT] = iowr_setx;
     vm->iowr[SETY_PORT] = iowr_sety;
-    w = NULL;
-    if (argc == 2) {
-        fp = fopen(argv[1], "r");
-        if (fp == NULL) {
-            fprintf(stderr, "Can't open %s\n", argv[1]);
-            return 1;
-        }
-        while (VM_steps(1000));
-        fclose(fp);
-    } else if (argc == 1) {
-        fp = NULL;
-        initscr(); cbreak(); noecho(); nl(); clear();
-        // border window
-        bw = newwin(WLINES+2, WCOLS+2, 0, 0);
-        wborder(bw, 0, 0, 0, 0, 0, 0, 0, 0);
-        wrefresh(bw);
-        // debug panel
-        dw = newwin(1, 30, LINES-1, COLS-30);
-        w = newwin(WLINES, WCOLS, 1, 1);
-        scrollok(w, 0);
-        while (VM_steps(1000)) {
-            debug_panel();
-        }
-        nocbreak(); echo(); delwin(w); delwin(bw); delwin(dw); endwin();
-        printf("\nDone!\n");
-        fprintf(stderr, "Done!\n");
-        VM_printdbg();
-    } else {
-        fprintf(stderr, "Usage: ./forth [filename]\n");
-        retcode = 1;
+    initscr(); cbreak(); noecho(); nl(); clear();
+    // border window
+    bw = newwin(WLINES+2, WCOLS+2, 0, 0);
+    wborder(bw, 0, 0, 0, 0, 0, 0, 0, 0);
+    wrefresh(bw);
+    // debug panel
+    dw = newwin(1, 30, LINES-1, COLS-30);
+    w = newwin(WLINES, WCOLS, 1, 1);
+    scrollok(w, 0);
+    while (VM_steps(1000)) {
+        debug_panel();
     }
+    nocbreak(); echo(); delwin(w); delwin(bw); delwin(dw); endwin();
+    printf("\nDone!\n");
+    fprintf(stderr, "Done!\n");
+    VM_printdbg();
     VM_deinit();
-    return retcode;
+    return 0;
 }
